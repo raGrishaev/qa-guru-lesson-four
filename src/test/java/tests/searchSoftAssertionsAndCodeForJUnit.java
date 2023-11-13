4b7ff5359bed7e7a32cb14142d94b289e8148663
Roman Grishaev <grishaev.r-a@ya.ru>
2023-11-14 00:09:33 +0300

    added test for selenide in git hub

---
+++ src/test/java/tests/searchSoftAssertionsAndCodeForJUnit.java
@@ -0,0 +1,44 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class searchSoftAssertionsAndCodeForJUnit {
+
+    @BeforeAll
+    public static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl="https://github.com";
+        Configuration.holdBrowserOpen=false;
+        Configuration.pageLoadStrategy = "eager";
+
+    }
+
+    @Test
+    void searchSoftAssertionsAndCodeForJUnit(){
+
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".js-wiki-more-pages-link").click();
+        $("[data-filterable-for=wiki-pages-filter]").shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click();
+        $("#user-content-3-using-junit5-extend-test-class").sibling(0).$("pre").
+                shouldHave(text("""
+                        @ExtendWith({SoftAssertsExtension.class})
+                        class Tests {
+                         @Test
+                        void test() {
+                        Configuration.assertionMode = SOFT;
+                        open("page.html");
+
+                        $("#first").should(visible).click();
+                        $("#second").should(visible).click();
+                        }
+                        }"""));
+    }
+}
